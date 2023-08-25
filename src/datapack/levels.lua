ceaf9fdb67effb7e299576d5a78d29126f5c793f
ActivexDiamond <activexdiamond@gmail.com>
2023-08-25 09:32:14 +0300

    Polish level info and fix some typos.

--- src/datapack/levels.lua
+++ src/datapack/levels.lua
@@ -1,39 +1,67 @@
 data{"level1",
 	name = "Hello, World!",
-	skills = {"nothing", "deflect", "destroy"},
+	skills = {"dissipate"},
 	signs = {
-		{6, 3, "You awaken in a dark dungeon...\nYour feet shackled...\nWith nothing but your trusty spellbook;\nHolding only a teleport spell."},
-		{2, 18, "Use [Left Mouse Button] to shoot your teleportation orb.\nTry to get it in the target zone."},
+		{5, 0.5, "You awaken in a dark dungeon...\nYour feet shackled...\nWith nothing but your trusty Book Of Shadows;\nHolding only a teleport spell."},
+		{1, 10, "Use [Left Mouse Button] to shoot your teleportation orb.\nTry to get it in the portal."},
 	},
 }
 
 data{"level2",
 	name = "Literacy",
-	skills = {"nothing", "deflect", "destroy"},
+	skills = {"dissipate", "destroy"},
 	signs = {
-		{6, 3, "Press [E] to bring up your spellbook.\nUse it to change the collision-rules of your orb!."},
+		{1, 9.8, "Press [E] to bring up your Book Of Shadows.\nUse it to change the collision-rules of your orb!\nYou can't shoot while its open."},
 	},
 }
 
 data{"level3",
-	name = "Breaker",
-	skills = {"nothing", "deflect", "destroy"},
+	name = "Bouncy",
+	skills = {"dissipate", "deflect"},
 	signs = {
---		{6, 3, "center."},
+		{1, 10.8, "Use a combination of your spellbook-trickery and aim to complete each level."},
 	},
 }
 
 data{"level4",
-	name = "Bouncy",
-	skills = {"nothing", "deflect", "destroy"},
+	name = "Mix N' Match",
+	skills = {"dissipate", "deflect", "destroy"},
+	signs = {
+		{1, 10.8, "Good luck!"},
+	},
+}
+
+data{"level5",
+	name = "Grabbing The Ropes",
+	skills = {"dissipate", "deflect", "destroy"},
+}
+
+data{"level6",
+	name = "Grabbing The Ropes",
+	skills = {"dissipate", "deflect", "destroy"},
+}
+
+data{"level7",
+	name = "Ice Physics?",
+	skills = {"dissipate", "slide"},
+}
+
+data{"level8",
+	name = "Precise",
+	skills = {"dissipate", "deflect", "slide"},
+}
+
+data{"level_win",
+	name = "Vitcory: Broken Shackles",
+	skills = {"dissipate", "deflect", "destroy", "slide"},
 	signs = {
 --		{6, 3, "center."},
 	},
 }
 
-data{"level5",
-	name = "NAME",
-	skills = {"nothing", "deflect", "destroy"},
+data{"level_test",
+	name = "Broken Shackles",
+	skills = {"dissipate", "deflect", "destroy"},
 	signs = {
 --		{6, 3, "center."},
 	},
@@ -41,7 +69,7 @@ data{"level5",
 
 data{"blank",
 	name = "NAME",
-	skills = {"nothing", "deflect", "destroy"},
+	skills = {"dissipate", "deflect", "destroy"},
 	signs = {
 --		{6, 3, "center."},
 	},
