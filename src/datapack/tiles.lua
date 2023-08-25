99a3d43a74bc2b2b91f1ad5830f85f604234d6b3
ActivexDiamond <activexdiamond@gmail.com>
2023-08-25 09:31:36 +0300

    Add a bunch more tiles, with art.

--- src/datapack/tiles.lua
+++ src/datapack/tiles.lua
@@ -1,15 +1,25 @@
-data{"stone_tile",
+data{"void_tile",
 	w = 32, h = 32,
+	name = 'Onyx Tile',
 }
 
-
-data{"wood_tile",
+data{"amethyst_tile",
 	w = 32, h = 32,
+	name = 'Amethyst Tile',
 }
 
-
-data{"dirt_tile",
+data{"carnelian_tile",
 	w = 32, h = 32,
+	name = 'Carnelian Tile',
 }
 
+data{"emerald_tile",
+	w = 32, h = 32,
+	name = 'Emerald Tile',
+}
+
+data{"lapis_tile",
+	w = 32, h = 32,
+	name = 'Lapis Tile',
+}
 
