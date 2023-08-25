3ef1988e074ebd4b0d7a48a73e2cfb0bdaf3dac1
ActivexDiamond <activexdiamond@gmail.com>
2023-08-25 09:31:54 +0300

    Add stats for win-level movement.

--- src/datapack/mobs.lua
+++ src/datapack/mobs.lua
@@ -1,15 +1,24 @@
 data{"wizard",
 	speed = 250,
 	projectileSpeed = 350,
-	w = 32, h = 32
+	jumpPower = 500,
+	jumpFrames = 10,
+	jumpCooldown = -25,
+	gravity = 200,
+	w = 32, h = 32,
+}
+
+data{"spellbook",
+	projectileSpeed = 350,
 }
 
 data{"projectile",
-	w = 4, h = 4
+	w = 16, h = 16,
 }
 
 
 
 data{"goal",
-	w = 32, h = 32
+	w = 32, h = 32,
 }
+
